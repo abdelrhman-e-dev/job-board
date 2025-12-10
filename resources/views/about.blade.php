a0740af804ae968ad69a71d981c039c5619e956e
Abdelrhman <abdelrhmanroshdy8@gmail.com>
2025-12-10 21:59:28 +0200

    Add About and Contact blade views with reusable navigation components

---
+++ resources/views/about.blade.php
@@ -0,0 +1,8 @@
+@extends('layout.layout')
+@section('title' , 'About')
+@section('page_content')
+    <x-main-nav></x-main-nav>
+    <h1 class="text-3xl font-bold underline">
+        Hello About!
+    </h1>
+@endsection
---
+++ resources/views/contact.blade.php
@@ -0,0 +1,8 @@
+@extends('layout.layout')
+@section('title' , 'Contact')
+@section('page_content')
+    <x-main-nav></x-main-nav>
+    <h1 class="text-3xl font-bold underline">
+        Hello contact!
+    </h1>
+@endsection
