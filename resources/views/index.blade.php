9a212a17a31d9fee964e35bc92cd408f654ca875
Abdelrhman <abdelrhmanroshdy8@gmail.com>
2025-12-10 21:52:50 +0200

    rename welcome.blade.php to index.blade.php

---
+++ resources/views/index.blade.php
@@ -0,0 +1,8 @@
+@extends('layout.layout')
+@section('title' , 'Home')
+@section('page_content')
+    <x-main-nav></x-main-nav>
+    <h1 class="text-3xl font-bold underline">
+        Hello world!
+    </h1>
+@endsection
