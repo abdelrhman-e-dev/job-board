0ed4c5501a0e3cf5851121fc2ccf66ab93e2f669
Abdelrhman <abdelrhmanroshdy8@gmail.com>
2026-01-17 16:32:46 +0200

    Refactor Experience model for improved readability and add migration for experiences table with foreign key constraints

--- job-backoffice/app/Models/Experience.php
+++ job-backoffice/app/Models/Experience.php
@@ -10,24 +10,29 @@ use Illuminate\Database\Eloquent\Model;
 
 class Experience extends Model
 {
-    use HasFactory, HasUlids, SoftDeletes;
+  use HasFactory, HasUlids, SoftDeletes;
 
-    protected $table = 'experiences';
-    protected $keyType = 'string';
-    public $incrementing = false;
-    protected $primaryKey = 'experience_id';
-    protected $fillable = [
-        'user_id',
-        'job_title',
-        'company',
-        'start_date',
-        'end_date',
-        'is_current',
-        'description',
-    ];
-    // relation between Experience and User
-    public function user()
-    {
-        return $this->belongsTo(User::class, 'user_id', 'user_id');
-    }  
+  protected $table = 'experiences';
+  protected $keyType = 'string';
+  public $incrementing = false;
+  protected $primaryKey = 'experience_id';
+  protected $fillable = [
+    'user_id',
+    'job_title',
+    'company',
+    'start_date',
+    'end_date',
+    'is_current',
+    'description',
+  ];
+  protected $dates = [
+    'start_date',
+    'end_date',
+    'deleted_at',
+  ];
+  // relation between Experience and User
+  public function user()
+  {
+    return $this->belongsTo(User::class, 'user_id', 'user_id');
+  }
 }
