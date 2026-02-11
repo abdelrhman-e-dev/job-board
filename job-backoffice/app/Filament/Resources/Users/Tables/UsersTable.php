ab2c3613b8b849f5986de52b6e33d4cb992e0edf
Abdelrhman <abdelrhmanroshdy8@gmail.com>
2026-02-12 00:27:34 +0200

    Revert "feat: Create Filament UsersTable with columns, filters, and actions for user management."
    This reverts commit ac544006d344d178062b712e0cd1af5644e4afd6.

--- job-backoffice/app/Filament/Resources/Users/Tables/UsersTable.php
+++ job-backoffice/app/Filament/Resources/Users/Tables/UsersTable.php
@@ -47,6 +47,7 @@ class UsersTable
           ->badge()
           ->color(
             fn(string $state): string => match ($state) {
+              'system-admin' => 'danger',
               'company-owner' => 'info',
               'hiring-manager' => 'info',
               'recruiter' => 'gray',
@@ -74,24 +75,18 @@ class UsersTable
       ->filters([
         SelectFilter::make('role')
           ->options([
+            'system-admin' => 'System Admin',
             'company-owner' => 'Company Owner',
             'hiring-manager' => 'Hiring Manager',
             'recruiter' => 'Recruiter',
             'job-seeker' => 'Job Seeker',
           ]),
-        SelectFilter::make('status')
-          ->options([
-            'Active' => 'Active',
-            'Inactive' => 'Inactive',
-          ]),
-          
         TrashedFilter::make(),
       ])
       ->actions([
         ViewAction::make()
           ->modalHeading(fn($record) => $record->full_name)
           ->modalWidth('6xl')
-          ->visible(fn($record) => $record->role == 'job-seeker')
           ->form([
             Section::make('User Information')
               ->schema([
