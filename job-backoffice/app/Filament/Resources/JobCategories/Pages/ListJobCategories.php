b14bc6d6a17bb392b0c08e9d4d48ef7eaabb9542
Abdelrhman <abdelrhmanroshdy8@gmail.com>
2026-03-12 01:19:01 +0200

    feat: Add comprehensive Filament resources for job category management, including a new status column and related widgets.

--- job-backoffice/app/Filament/Resources/JobCategories/Pages/ListJobCategories.php
+++ job-backoffice/app/Filament/Resources/JobCategories/Pages/ListJobCategories.php
@@ -3,17 +3,32 @@
 namespace App\Filament\Resources\JobCategories\Pages;
 
 use App\Filament\Resources\JobCategories\JobCategoryResource;
+use App\Filament\Resources\JobCategories\Widgets\ApplicationsPerCategory;
+use App\Filament\Resources\JobCategories\Widgets\CategoryGrowthTrend;
+use App\Filament\Resources\JobCategories\Widgets\JobPostingsPerCategory;
+use App\Filament\Resources\JobCategories\Widgets\CategoryStats;
+use App\Filament\Resources\JobCategories\Widgets\TrendingCategories;
 use Filament\Actions\CreateAction;
 use Filament\Resources\Pages\ListRecords;
 
 class ListJobCategories extends ListRecords
 {
-    protected static string $resource = JobCategoryResource::class;
+  protected static string $resource = JobCategoryResource::class;
 
-    protected function getHeaderActions(): array
-    {
-        return [
-            CreateAction::make(),
-        ];
-    }
+  protected function getHeaderActions(): array
+  {
+    return [
+      CreateAction::make(),
+    ];
+  }
+  protected function getHeaderWidgets(): array
+  {
+    return [
+      CategoryStats::class,
+      JobPostingsPerCategory::class,
+      ApplicationsPerCategory::class,
+      TrendingCategories::class,
+      CategoryGrowthTrend::class,
+    ];
+  }
 }
