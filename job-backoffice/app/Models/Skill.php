55466f3783d255dc681affc1dd30da59cb549d9c
Abdelrhman <abdelrhmanroshdy8@gmail.com>
2026-03-14 08:08:31 +0200

    feat: Enhance job vacancy management by adding new fields, related models, and Filament resources.

--- job-backoffice/app/Models/Skill.php
+++ job-backoffice/app/Models/Skill.php
@@ -5,13 +5,10 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Concerns\HasUlids;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
-use Illuminate\Database\Eloquent\SoftDeletes;
 
 class Skill extends Model
 {
-    use HasFactory;
-    use SoftDeletes;
-    use HasUlids;
+    use HasFactory, HasUlids;
 
     protected $primaryKey = 'skill_id';
     public $incrementing = false;
