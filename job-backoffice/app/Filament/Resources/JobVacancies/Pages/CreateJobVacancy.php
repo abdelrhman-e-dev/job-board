769bab6c5fd43c165c8fc8473bee9d925d676ea4
Abdelrhman <abdelrhmanroshdy8@gmail.com>
2026-04-03 03:55:43 +0200

    feat: implement job posting limit validation for creating and editing vacancies and add vacancy lifecycle management actions

--- job-backoffice/app/Filament/Resources/JobVacancies/Pages/CreateJobVacancy.php
+++ job-backoffice/app/Filament/Resources/JobVacancies/Pages/CreateJobVacancy.php
@@ -3,13 +3,30 @@
 namespace App\Filament\Resources\JobVacancies\Pages;
 
 use App\Filament\Resources\JobVacancies\JobVacancyResource;
+use App\Models\Company;
+use Filament\Notifications\Notification;
 use Filament\Resources\Pages\CreateRecord;
+
 
 class CreateJobVacancy extends CreateRecord
 {
   protected static string $resource = JobVacancyResource::class;
   protected function getRedirectUrl(): string
   {
-    return static::getResource()::getUrl('index');
+    return static::getResource()::getUrl('index');  
+  }
+  protected function beforeCreate(): array
+  {
+    // check job post limit of the company before create the record
+    $company = Company::find($this->data['company_id']);
+    if ($company['job_posting_limit'] <= $company->jobs()->count()) {
+      Notification::make()
+        ->title('Job post limit reached')
+        ->body("{$company['name']} Company has reached the job post limit {$company['job_posting_limit']}")
+        ->danger()
+        ->send();
+      return $this->halt();
+    }
+    return $this->data;
   }
 }
--- job-backoffice/app/Filament/Resources/JobVacancies/Pages/EditJobVacancy.php
+++ job-backoffice/app/Filament/Resources/JobVacancies/Pages/EditJobVacancy.php
@@ -3,9 +3,11 @@
 namespace App\Filament\Resources\JobVacancies\Pages;
 
 use App\Filament\Resources\JobVacancies\JobVacancyResource;
+use App\Models\Company;
 use Filament\Actions\DeleteAction;
 use Filament\Actions\ForceDeleteAction;
 use Filament\Actions\RestoreAction;
+use Filament\Notifications\Notification;
 use Filament\Resources\Pages\EditRecord;
 
 class EditJobVacancy extends EditRecord
@@ -15,13 +17,46 @@ class EditJobVacancy extends EditRecord
   protected function getHeaderActions(): array
   {
     return [
-      DeleteAction::make(),
+      DeleteAction::make()
+        ->requiresConfirmation()
+        ->modalHeading('Delete Job Vacancy')
+        ->modalDescription('This action will move the job vacancy to the trash and it will be deleted after 30 days.')
+        ->modalSubmitActionLabel('Yes, delete it')
+        ->before(function ($record) {
+          $record->update([
+            'status' => 'trashed',
+          ]);
+        }),
       ForceDeleteAction::make(),
-      RestoreAction::make(),
+      RestoreAction::make()
+        ->requiresConfirmation()
+        ->modalHeading('Restore Job Vacancy')
+        ->modalDescription('This action will restore the job vacancy from the trash to draft.')
+        ->modalSubmitActionLabel('Yes, restore it')
+        ->after(function ($record) {
+          $record->update([
+            'status' => 'draft',
+          ]);
+        }),
     ];
   }
   protected function getRedirectUrl(): string
   {
     return static::getResource()::getUrl('index');
+  }
+  protected function beforeSave(): void
+  {
+    // check job post limit of the company before create/update the record
+    $company = Company::find($this->data['company_id']);
+
+    if ($company->job_posting_limit <= $company->jobs()->count()) {
+      Notification::make()
+        ->title('Job post limit reached')
+        ->body("{$company->name} Company has reached the job post limit {$company->job_posting_limit}")
+        ->danger()
+        ->send();
+      $this->halt();
+
+    }
   }
 }
