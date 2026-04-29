cc65a8f1c30129772715874ea007017fdeff66db
Abdelrhman <abdelrhmanroshdy8@gmail.com>
2026-04-29 20:56:44 +0300

    feat: add hired_at column to applications and implement status-based auto-timestamping in Filament form

--- job-backoffice/app/Filament/Resources/Applications/Pages/ListApplications.php
+++ job-backoffice/app/Filament/Resources/Applications/Pages/ListApplications.php
@@ -4,24 +4,27 @@ namespace App\Filament\Resources\Applications\Pages;
 
 use App\Filament\Resources\Applications\ApplicationResource;
 use App\Filament\Resources\Applications\Widgets\ApplicationToHire;
+use App\Filament\Resources\Applications\Widgets\TimeToHire;
 use Filament\Actions\CreateAction;
 use Filament\Resources\Pages\ListRecords;
 
 class ListApplications extends ListRecords
 {
-    protected static string $resource = ApplicationResource::class;
+  protected static string $resource = ApplicationResource::class;
 
-    protected function getHeaderActions(): array
-    {
-        return [
-            
-        ];
-    }
+  protected function getHeaderActions(): array
+  {
+    return [
 
-    public function getHeaderWidgets(): array
-    {
-        return [
-            ApplicationToHire::class
-        ];
-    }
+    ];
+  }
+
+  public function getHeaderWidgets(): array
+  {
+    return [
+      ApplicationToHire::class,
+      TimeToHire::class
+
+    ];
+  }
 }
