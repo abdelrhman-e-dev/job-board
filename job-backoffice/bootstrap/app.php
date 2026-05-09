26b8233c7b8f260a5cfd9df2e370b95436b2c9d3
Abdelrhman <abdelrhmanroshdy8@gmail.com>
2026-05-09 22:58:23 +0300

    feat: configure multi-guard authentication and register custom company routes and middleware

--- job-backoffice/bootstrap/app.php
+++ job-backoffice/bootstrap/app.php
@@ -1,18 +1,32 @@
 <?php
 
+use App\Http\Middleware\Company\CompanyApproved;
+use App\Http\Middleware\Company\CompanyAuth;
+use App\Http\Middleware\Company\CompanyRole;
 use Illuminate\Foundation\Application;
 use Illuminate\Foundation\Configuration\Exceptions;
 use Illuminate\Foundation\Configuration\Middleware;
 
 return Application::configure(basePath: dirname(__DIR__))
-    ->withRouting(
-        web: __DIR__.'/../routes/web.php',
-        commands: __DIR__.'/../routes/console.php',
-        health: '/up',
-    )
-    ->withMiddleware(function (Middleware $middleware): void {
-        //
-    })
-    ->withExceptions(function (Exceptions $exceptions): void {
-        //
-    })->create();
+  ->withRouting(
+    web: __DIR__ . '/../routes/web.php',
+    commands: __DIR__ . '/../routes/console.php',
+    health: '/up',
+    then: function () {
+      Route::prefix('company')->middleware(['CompanyRole', 'CompanyAuth', 'CompanyApproved'])
+        ->name('company.')
+        ->group(function () {
+          require __DIR__ . '/../routes/company.php';
+        });
+    }
+  )
+  ->withMiddleware(function (Middleware $middleware): void {
+    $middleware->alias([
+      'CompanyRole' => CompanyRole::class,
+      'CompanyAuth' => CompanyAuth::class,
+      'CompanyApproved' => CompanyApproved::class,
+    ]);
+  })
+  ->withExceptions(function (Exceptions $exceptions): void {
+    //
+  })->create();
