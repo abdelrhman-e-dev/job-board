13f016b2747ac8e1076572f3d3245f862e243958
Abdelrhman <abdelrhmanroshdy8@gmail.com>
2026-05-14 03:17:53 +0300

    feat: add Tailwind configuration, design documentation, and company authentication boilerplate

--- job-backoffice/app/Http/Requests/Company/Auth/LoginRequest.php
+++ job-backoffice/app/Http/Requests/Company/Auth/LoginRequest.php
@@ -3,26 +3,55 @@
 namespace App\Http\Requests\Company\Auth;
 
 use Illuminate\Foundation\Http\FormRequest;
+use Illuminate\Support\Facades\RateLimiter;
+use Illuminate\Support\Str;
+use Illuminate\Validation\ValidationException;
 
 class LoginRequest extends FormRequest
 {
-    /**
-     * Determine if the user is authorized to make this request.
-     */
-    public function authorize(): bool
-    {
-        return true;
+  /**
+   * Determine if the user is authorized to make this request.
+   */
+  public function authorize(): bool
+  {
+    return true;
+  }
+
+  /**
+   * Get the validation rules that apply to the request.
+   *
+   * @return array<string, \Illuminate\Contracts\Validation\ValidationRule|array<mixed>|string>
+   */
+  public function rules(): array
+  {
+    return [
+      'email' => ['required', 'email'],
+      'password' => ['required', 'string', 'min:8']
+    ];
+  }
+  public function authenticate(): void
+  {
+    $this->ensureIsNotRateLimited();
+  }
+
+  public function ensureIsNotRateLimited(): void
+  {
+    if (!RateLimiter::tooManyAttempts($this->throttleKey(), 3)) {
+      return;
     }
 
-    /**
-     * Get the validation rules that apply to the request.
-     *
-     * @return array<string, \Illuminate\Contracts\Validation\ValidationRule|array<mixed>|string>
-     */
-    public function rules(): array
-    {
-        return [
-            //
-        ];
-    }
+    $seconds = RateLimiter::availableIn($this->throttleKey());
+
+    throw ValidationException::withMessages([
+      'email' => trans('auth.throttle', [
+        'seconds' => $seconds,
+        'minutes' => ceil($seconds / 60),
+      ]),
+    ]);
+  }
+
+  public function throttleKey(): string
+  {
+    return Str::lower($this->input('email')) . '|' . $this->ip();
+  }
 }
