06ca4fc583696f86e23fd63f0dbec5a56238631f
Abdelrhman <abdelrhmanroshdy8@gmail.com>
2026-05-23 00:38:02 +0300

    feat: implement company registration flow with multi-step Livewire form and email verification

--- job-backoffice/app/Http/Controllers/Company/Auth/RegisterController.php
+++ job-backoffice/app/Http/Controllers/Company/Auth/RegisterController.php
@@ -3,17 +3,40 @@
 namespace App\Http\Controllers\Company\Auth;
 
 use App\Http\Controllers\Controller;
+
+use App\Http\Requests\Company\Auth\RegistrationRequest;
+use App\Models\User;
+use DB;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Hash;
 
 class RegisterController extends Controller
 {
   public function show()
   {
-    return "<h1>hello register</h1>";
+    return view('company.auth.register');
   }
 
-  public function store(Request $request)
+  public function store(RegistrationRequest $request)
   {
-    return "<h1>hello register post</h1>";
+    $data = $request->validated();
+    // create user account 
+    DB::transaction(function () use ($data) {
+      $user = $this->createUser($data);
+    });
+  }
+
+
+  protected function createUser($data)
+  {
+    return User::create([
+      'first_name' => $data['first_name'],
+      'last_name' => $data['last_name'],
+      'email' => $data['email'],
+      'password' => Hash::make($data['password']),
+      'company_id' => null,
+      'role_id' => User::ROLES['company-owner'],
+      // status by default is Inactive
+    ]);
   }
 }
