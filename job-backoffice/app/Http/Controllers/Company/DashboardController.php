0c66f6908d10bdb0d64d4a2feb5218ccea75d6ba
Abdelrhman <abdelrhmanroshdy8@gmail.com>
2026-05-30 00:05:18 +0300

    feat: implement company dashboard controller, service, and repository for statistics and profile completion tracking

--- job-backoffice/app/Http/Controllers/Company/DashboardController.php
+++ job-backoffice/app/Http/Controllers/Company/DashboardController.php
@@ -3,9 +3,22 @@
 namespace App\Http\Controllers\Company;
 
 use App\Http\Controllers\Controller;
+use App\Services\Company\DashboardService;
 use Illuminate\Http\Request;
 
 class DashboardController extends Controller
 {
-    //
+  public function __construct(private DashboardService $service)
+  {
+
+  }
+  public function index()
+  {
+    return view('company.dashboard.index', [
+      'stats' => $this->service->getStats(),
+      'recentApplications' => $this->service->getRecentApplications(),
+      'missingData' => $this->service->getMissingData()['missing'],
+      'missingPercentage' => $this->service->getMissingData()['percentage'],
+    ]);
+  }
 }
