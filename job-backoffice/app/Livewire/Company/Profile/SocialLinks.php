4190e6602aeaa014daf14c2a66b99e8e8fe674b3
Abdelrhman <abdelrhmanroshdy8@gmail.com>
2026-06-09 03:03:41 +0300

    feat: implement company profile management with social links component and supporting service layer

---
+++ job-backoffice/app/Livewire/Company/Profile/SocialLinks.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Livewire\Company\Profile;
+
+use Livewire\Component;
+
+class SocialLinks extends Component
+{
+    public function render()
+    {
+        return view('livewire.company.profile.social-links');
+    }
+}
